9422c1a44ff49f2f9e0b6e533085e3941b5502d6
Emile Renaud <emile.renaud@krag.io>
2022-04-04 10:16:21 -0400

    2 pushbuttons on ISR

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include <tables/square_no_alias_2048_int8.h>
 
 #include "pcm_audio.hpp"
+#include <limits.h>
+#include <event_groups.h>
+
 
 using Sawtooth = Oscil<SAW2048_NUM_CELLS, SAMPLE_RATE>;
 using SquareWv = Oscil<SQUARE_NO_ALIAS_2048_NUM_CELLS, SAMPLE_RATE>;
@@ -16,8 +19,17 @@ using SquareWv = Oscil<SQUARE_NO_ALIAS_2048_NUM_CELLS, SAMPLE_RATE>;
 #define PIN_RV3 A2
 #define PIN_RV4 A3
 
+// Variables
 int potValue[4] = {0,0,0,0};
+EventGroupHandle_t eventGroupButton;
+
+
+// Prototypes
 void readPotentiometer(void *);
+void ISR_button1(void);
+void ISR_button2(void);
+void waitButton(void *);
+
 
 SquareWv squarewv_;
 Sawtooth sawtooth_;
@@ -58,17 +70,29 @@ void setup()
 
     pcmSetup();
 
-    Serial.println("Synth prototype ready");
+    eventGroupButton = xEventGroupCreate();
+
+    attachInterrupt(digitalPinToInterrupt(PIN_SW1),ISR_button1,CHANGE);
+    attachInterrupt(digitalPinToInterrupt(PIN_SW2),ISR_button2,CHANGE);
 
     xTaskCreate(readPotentiometer,"readPot",128,NULL,2,NULL);
+
+    xTaskCreate(waitButton,"waitButton",128,NULL,2,NULL);
+
+
+    Serial.println("Synth prototype ready");
 }
 
 void loop()
 {    
-    digitalWrite(LED_BUILTIN, 1);
-    delay(500);
-    digitalWrite(LED_BUILTIN, 0);
-    delay(500);
+    // xEventGroupWaitBits(
+    //                 eventGroupButton,
+    //                 0x01,
+    //                 pdTRUE,
+    //                 pdFALSE,
+    //                 portMAX_DELAY);
+    //     // xTaskNotifyWait(0x00,ULONG_MAX,&ulNotifyValue,portMAX_DELAY);
+    //     Serial.println("Waked up 2");
 }
 
 void readPotentiometer(void *)
@@ -78,9 +102,47 @@ void readPotentiometer(void *)
         for(int i=0; i<4; i++)
         {
         potValue[i] = analogRead(i);
-        Serial.print(potValue[i]);
+        // Serial.print(potValue[i]);
         }
         // Serial.println();
-        vTaskDelay(100/portTICK_PERIOD_MS);
+        vTaskDelay(100/portTICK_PERIOD_MS); // 10Hz -> 100ms
+    }
+}
+
+void ISR_button1()
+{
+    if(digitalRead(PIN_SW1))
+    {
+        xEventGroupSetBitsFromISR(eventGroupButton,0x01,pdFALSE);
+    }
+    else
+    {
+        xEventGroupClearBitsFromISR(eventGroupButton,0x01);
+    }
+}
+
+void ISR_button2()
+{
+    if(digitalRead(PIN_SW2))
+    {
+        xEventGroupSetBitsFromISR(eventGroupButton,0x02,pdFALSE);
+    }
+    else
+    {
+        xEventGroupClearBitsFromISR(eventGroupButton,0x02);
+    }
+}
+
+void waitButton(void *)
+{
+    for(;;)
+    {
+        // xEventGroupWaitBits(
+        //             eventGroupButton,
+        //             0x03,    // Check bit 0 and 1.
+        //             pdTRUE,  // Clear bit on exit.
+        //             pdTRUE, // Wait all the bits
+        //             portMAX_DELAY); // no Timeout
+        // Serial.println("Waked up 2");
     }
 }
