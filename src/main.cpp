3671852cc9e1f7a6d05c38a0983c544c9fb21343
Thomas Landry <itomlan@icloud.com>
2022-04-05 12:05:16 -0400

    Modif consult

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,10 @@ using SquareWv = Oscil<SQUARE_NO_ALIAS_2048_NUM_CELLS, SAMPLE_RATE>;
 
 // Variables
 int potValue[4] = {0,0,0,0};
+float q_org;
+float f_org;
+bool initMel = 0;
+bool mel_ON = 0;
 EventGroupHandle_t eventGroupButton;
 
 
@@ -51,10 +55,12 @@ int8_t nextSample()
     int8_t vco = sawtooth_.next() + squarewv_.next();
     
     // VCF 
+    //int8_t vcf = processVCF(vco);
     int8_t vcf = vco;
 
-    // VCA (disabled)   
-    int8_t vca = vcf;
+    // VCA (disabled)
+    int8_t vca = processVCA(vcf);   
+    //int8_t vca = vcf;
 
     int8_t output = vca;
 
@@ -68,7 +74,7 @@ void setup()
     
     Serial.begin(9600);
 
-    // Oscillator.
+    // Oscillator
     squarewv_ = SquareWv(SQUARE_NO_ALIAS_2048_DATA);
     sawtooth_ = SquareWv(SAW2048_DATA);
     setNoteHz(110.0);
@@ -80,10 +86,10 @@ void setup()
     attachInterrupt(digitalPinToInterrupt(PIN_SW1),ISR_button1,CHANGE);
     attachInterrupt(digitalPinToInterrupt(PIN_SW2),ISR_button2,CHANGE);
 
-    xTaskCreate(readPotentiometer,"readPot",128,NULL,2,NULL);
+    xTaskCreate(readPotentiometer,"readPot",128,NULL,3,NULL);
 
     // xTaskCreate(waitButton,"waitButton",128,NULL,2,NULL);
-    xTaskCreate(melodie,"melodie", 500, NULL, 3, NULL);
+    xTaskCreate(melodie,"melodie", 500, NULL, 2, NULL);
 
 
     Serial.println("Synth prototype ready");
@@ -110,7 +116,9 @@ void readPotentiometer(void *)
         potValue[i] = analogRead(i);
         // Serial.print(potValue[i]);
         }
-        // Serial.println();
+        q_org = (potValue[1]*1.0f)/1023.0f;
+        f_org = (potValue[2]*(1000.0f*((2.0f*PI)/8000.0f)))/1023.0f;
+        //Serial.println(potValue[1]);
         vTaskDelay(100/portTICK_PERIOD_MS); // 10Hz -> 100ms
     }
 }
@@ -121,11 +129,14 @@ void ISR_button1()
     {
         pcmSetup();
         xEventGroupSetBitsFromISR(eventGroupButton,0x01,pdFALSE);
+        initMel = 0;
+        mel_ON = 1;
     }
     else
     {
-        xEventGroupClearBitsFromISR(eventGroupButton,0x01);
-        stopPlayback();
+        mel_ON = 0;
+        //xEventGroupClearBitsFromISR(eventGroupButton,0x01);
+        //stopPlayback();
     }
 }
 
@@ -170,12 +181,12 @@ int8_t processVCF(int8_t vco)
         }
     }
 
-    float q_org = (potValue[1]*1.0f)/1023.0f;
-    float f_org = (potValue[2]*(4000.0f*((2.0f*PI)/8000.0f)))/1023.0f;
+    //float q_org = (potValue[1]*1.0f)/1023.0f;
+    //float f_org = (potValue[2]*(4000.0f*((2.0f*PI)/8000.0f)))/1023.0f;
     int16_t q = q_org*256;
     int16_t f = f_org*256;   
     int16_t fb = q + q/(1+f);
-    result = (f^2)*x + (2-2*f+f*fb-(f^2)*fb)*y[1] - (1-2*f+f*fb+(f^2)-(f^2)*fb)*y[2];
+    result = (f^2)*x - (2-2*f+f*fb-(f^2)*fb)*y[1] + (1-2*f+f*fb+(f^2)-(f^2)*fb)*y[2];
     y[0] = 0xFF & (result >> 8);
 
     return y[0];
@@ -183,50 +194,70 @@ int8_t processVCF(int8_t vco)
 
 int8_t processVCA(int8_t vcf)
 {
+    float tempsVCA_org = 1023.0f - ((potValue[3]*5000.0f)/1023.0f);
+    int16_t tempsVCA = tempsVCA_org*256;
+    int16_t resVCA = 0;
+    int8_t vcaOUT = 0;
 
+    if(mel_ON == 0)
+    {
+        int16_t decline = (tempsVCA - xTaskGetTickCount())/tempsVCA;
+        Serial.println(tempsVCA);
+        resVCA = decline*vcf;
+        vcaOUT = 0xFF & (resVCA >> 8);    
+    }
+    else
+    {
+        vcaOUT = vcf;
+    }
+
+     return vcaOUT;
 }
 
 void melodie(void*)
 {
     uint16_t nb_sample; // 512 max
     int8_t note = 0;
-    int8_t init = 0; 
-    for(;;)
-    {   
-        xEventGroupWaitBits(eventGroupButton, 0x01, pdFALSE, pdFALSE, portMAX_DELAY);
-
-        if(init == 0)
-        {
-            nb_sample = ((128*TEMPO_16T_MS)/16)*song[note].duration;
-            // Serial.print(nb_sample);
-            // Serial.print("      ");
-            // Serial.println(song[note].freq);
-            setNoteHz(song[note].freq);
-            init = 1;
-        }
-
-        if(!pcmBufferFull() && nb_sample != 0)
-        {
-            pcmAddSample(nextSample());
-            nb_sample--;
-            // Serial.println(nb_sample);
-            if(nb_sample == 0)
-            {
-
-                // Serial.print("sample = 0");
-                note++;
-
-                if(note == 4)
-                {
-                    note = 0;
-                }
-
-                init = 0;
-            }     
-        }
-
-        // vTaskDelay(0);
-    }
+    //int8_t init = 0; 
     
+    for(;;)
+    {
+        //if(mel_ON == 1)
+        //{   
+            xEventGroupWaitBits(eventGroupButton, 0x01, pdFALSE, pdFALSE, portMAX_DELAY);
+
+            if(initMel == 0)
+            {
+                nb_sample = ((128*TEMPO_16T_MS)/16)*song[note].duration;
+                // Serial.print(nb_sample);
+                // Serial.print("      ");
+                // Serial.println(song[note].freq);
+                setNoteHz(song[note].freq);
+                initMel = 1;
+            }
+
+            if(!pcmBufferFull() && nb_sample != 0)
+            {
+                pcmAddSample(nextSample());
+                nb_sample--;
+                // Serial.println(nb_sample);
+                if(nb_sample == 0)
+                {
+
+                    // Serial.print("sample = 0");
+                    note++;
+
+                    if(note == 4)
+                    {
+                        note = 0;
+                    }
+
+                    initMel = 0;
+                }     
+            }
+
+        //}
+        vTaskDelay(0);
+    }
 
 }
